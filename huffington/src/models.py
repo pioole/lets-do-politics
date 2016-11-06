cccf354df56c52a7189f8df4c0fa09d56d346c6f
Piotr Oleksy <piotr.olek@o2.pl>
2016-11-06 13:47:23 +0100

    updated db model

--- huffington/src/models.py
+++ huffington/src/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 
-class Article(models.Model):
+class Author(models.Model):
     def __str__(self):
         return self.name
 
     name = models.CharField(max_length=264, blank=False)
+
+
+class ArticleOrComment(models.Model):
+    author = models.ForeignKey(Author)
+    date = models.DateField()
+    parent = models.ForeignKey('self')
+    is_article = models.BooleanField()
+
+
